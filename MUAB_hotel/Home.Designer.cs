c8f9745224407a09f0cb1d9dfab04e8834f4fddd
Mustafa Abdulle <mustafa.abdulle@student.nbi-handelsakademin.se>
2022-12-21 19:42:51 +0100

    firts commit

---
+++ MUAB_hotel/Home.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace MUAB_hotel
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlMenuBar = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnReception = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnBooking = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlCloseBar = new System.Windows.Forms.Panel();
+            this.pBCloseBar = new System.Windows.Forms.PictureBox();
+            this.pnlMenuBarIcon = new System.Windows.Forms.Panel();
+            this.pBMenuBarIcon = new System.Windows.Forms.PictureBox();
+            this.pnlMenuBar.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.pnlCloseBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBCloseBar)).BeginInit();
+            this.pnlMenuBarIcon.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBMenuBarIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1025, 638);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlMenuBar
+            // 
+            this.pnlMenuBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.pnlMenuBar.Controls.Add(this.label1);
+            this.pnlMenuBar.Controls.Add(this.panel7);
+            this.pnlMenuBar.Controls.Add(this.panel6);
+            this.pnlMenuBar.Controls.Add(this.panel5);
+            this.pnlMenuBar.Controls.Add(this.panel4);
+            this.pnlMenuBar.Controls.Add(this.panel3);
+            this.pnlMenuBar.Controls.Add(this.panel2);
+            this.pnlMenuBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenuBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenuBar.Name = "pnlMenuBar";
+            this.pnlMenuBar.Size = new System.Drawing.Size(192, 633);
+            this.pnlMenuBar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 546);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel7.Controls.Add(this.btnLogout);
+            this.panel7.Location = new System.Drawing.Point(4, 566);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(176, 67);
+            this.panel7.TabIndex = 1;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.btnLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Image = ((System.Drawing.Image)(resources.GetObject("btnLogout.Image")));
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(-4, -6);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnLogout.Size = new System.Drawing.Size(187, 82);
+            this.btnLogout.TabIndex = 0;
+            this.btnLogout.Text = "        Home";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnSetting);
+            this.panel6.Location = new System.Drawing.Point(1, 288);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(176, 67);
+            this.panel6.TabIndex = 1;
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.btnSetting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSetting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetting.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting.ForeColor = System.Drawing.Color.White;
+            this.btnSetting.Image = ((System.Drawing.Image)(resources.GetObject("btnSetting.Image")));
+            this.btnSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.Location = new System.Drawing.Point(-4, -6);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnSetting.Size = new System.Drawing.Size(187, 82);
+            this.btnSetting.TabIndex = 0;
+            this.btnSetting.Text = "        Setting";
+            this.btnSetting.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.UseVisualStyleBackColor = false;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnReception);
+            this.panel5.Location = new System.Drawing.Point(1, 217);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(176, 67);
+            this.panel5.TabIndex = 1;
+            // 
+            // btnReception
+            // 
+            this.btnReception.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.btnReception.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReception.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReception.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReception.ForeColor = System.Drawing.Color.White;
+            this.btnReception.Image = ((System.Drawing.Image)(resources.GetObject("btnReception.Image")));
+            this.btnReception.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReception.Location = new System.Drawing.Point(-4, -6);
+            this.btnReception.Name = "btnReception";
+            this.btnReception.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnReception.Size = new System.Drawing.Size(195, 82);
+            this.btnReception.TabIndex = 0;
+            this.btnReception.Text = "        Reception";
+            this.btnReception.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReception.UseVisualStyleBackColor = false;
+            this.btnReception.Click += new System.EventHandler(this.btnReception_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnBooking);
+            this.panel4.Location = new System.Drawing.Point(1, 146);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(176, 67);
+            this.panel4.TabIndex = 1;
+            // 
+            // btnBooking
+            // 
+            this.btnBooking.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.btnBooking.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBooking.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBooking.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBooking.ForeColor = System.Drawing.Color.White;
+            this.btnBooking.Image = ((System.Drawing.Image)(resources.GetObject("btnBooking.Image")));
+            this.btnBooking.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBooking.Location = new System.Drawing.Point(-4, -6);
+            this.btnBooking.Name = "btnBooking";
+            this.btnBooking.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnBooking.Size = new System.Drawing.Size(187, 82);
+            this.btnBooking.TabIndex = 0;
+            this.btnBooking.Text = "        Booking";
+            this.btnBooking.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBooking.UseVisualStyleBackColor = false;
+            this.btnBooking.Click += new System.EventHandler(this.btnBooking_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnHome);
+            this.panel3.Location = new System.Drawing.Point(1, 75);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(176, 67);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(-4, -6);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnHome.Size = new System.Drawing.Size(187, 82);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "        Home";
+            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.UseVisualStyleBackColor = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pnlCloseBar);
+            this.panel2.Controls.Add(this.pnlMenuBarIcon);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel2.Location = new System.Drawing.Point(0, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(187, 67);
+            this.panel2.TabIndex = 0;
+            // 
+            // pnlCloseBar
+            // 
+            this.pnlCloseBar.Controls.Add(this.pBCloseBar);
+            this.pnlCloseBar.Location = new System.Drawing.Point(148, 15);
+            this.pnlCloseBar.Name = "pnlCloseBar";
+            this.pnlCloseBar.Size = new System.Drawing.Size(20, 21);
+            this.pnlCloseBar.TabIndex = 0;
+            // 
+            // pBCloseBar
+            // 
+            this.pBCloseBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pBCloseBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBCloseBar.Image = ((System.Drawing.Image)(resources.GetObject("pBCloseBar.Image")));
+            this.pBCloseBar.Location = new System.Drawing.Point(0, 0);
+            this.pBCloseBar.Name = "pBCloseBar";
+            this.pBCloseBar.Size = new System.Drawing.Size(20, 21);
+            this.pBCloseBar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBCloseBar.TabIndex = 0;
+            this.pBCloseBar.TabStop = false;
+            this.pBCloseBar.Click += new System.EventHandler(this.pBCloseBar_Click);
+            // 
+            // pnlMenuBarIcon
+            // 
+            this.pnlMenuBarIcon.Controls.Add(this.pBMenuBarIcon);
+            this.pnlMenuBarIcon.Location = new System.Drawing.Point(4, 10);
+            this.pnlMenuBarIcon.Name = "pnlMenuBarIcon";
+            this.pnlMenuBarIcon.Size = new System.Drawing.Size(41, 38);
+            this.pnlMenuBarIcon.TabIndex = 0;
+            // 
+            // pBMenuBarIcon
+            // 
+            this.pBMenuBarIcon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pBMenuBarIcon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBMenuBarIcon.Image = ((System.Drawing.Image)(resources.GetObject("pBMenuBarIcon.Image")));
+            this.pBMenuBarIcon.Location = new System.Drawing.Point(0, 0);
+            this.pBMenuBarIcon.Name = "pBMenuBarIcon";
+            this.pBMenuBarIcon.Size = new System.Drawing.Size(41, 38);
+            this.pBMenuBarIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBMenuBarIcon.TabIndex = 0;
+            this.pBMenuBarIcon.TabStop = false;
+            this.pBMenuBarIcon.Click += new System.EventHandler(this.pBMenuBarIcon_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 633);
+            this.Controls.Add(this.pnlMenuBar);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(854, 533);
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.pnlMenuBar.ResumeLayout(false);
+            this.pnlMenuBar.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.pnlCloseBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBCloseBar)).EndInit();
+            this.pnlMenuBarIcon.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBMenuBarIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlMenuBar;
+        private System.Windows.Forms.PictureBox pBMenuBarIcon;
+        private System.Windows.Forms.Panel pnlCloseBar;
+        private System.Windows.Forms.PictureBox pBCloseBar;
+        private System.Windows.Forms.Panel pnlMenuBarIcon;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnReception;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnBooking;
+    }
+}
+
---
+++ MUAB_hotel/Home.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MUAB_hotel
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            closeMenu();
+        }
+
+        #region Menu Bar
+
+        //Open Menu bar
+        public void openMenu()
+        {
+            pnlMenuBarIcon.Visible = false;
+            pnlMenuBar.Width = 185;
+        }
+
+        //Close Menu bar
+        public void closeMenu()
+        {
+            pnlMenuBarIcon.Visible = true;
+            pnlMenuBar.Width = 60;
+
+        }
+
+
+        #endregion
+
+
+        private void pBCloseBar_Click(object sender, EventArgs e)
+        {
+            closeMenu();
+        }
+
+        private void pBMenuBarIcon_Click(object sender, EventArgs e)
+        {
+            openMenu();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBooking_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReception_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
